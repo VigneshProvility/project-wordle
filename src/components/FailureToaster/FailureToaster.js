aec0c6485786d3df23692a94d4665147ae1a0695
VigneshProvility <vignesh@provility.com>
2024-11-28 09:17:01 +0530

    Code refactored

--- src/components/FailureToaster/FailureToaster.js
+++ src/components/FailureToaster/FailureToaster.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 function FailureToasterComponent(props) {
-    return <>
-        <div className="sad banner">
-            <p>Sorry, the correct answer is <strong>{props.answer}</strong>.</p>
-        </div>
-    </>
+    return <div className="sad banner">
+        <p>Sorry, the correct answer is <strong>{props.answer}</strong>.</p>
+    </div>
 }
 
 export default FailureToasterComponent;
--- src/components/SuccessToaster/SuccessToaster.js
+++ src/components/SuccessToaster/SuccessToaster.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 function SuccessToasterComponent(props) {
-    return <>
-        <div className="happy banner">
-            <p>
-                <strong>Congratulations!</strong> Got it in
-                <strong>{props.count + 1} guesses</strong>.
-            </p>
-        </div>
-    </>
+    return <div className="happy banner">
+        <p>
+            <strong>Congratulations!</strong> Got it in
+            <strong> {props.count + 1} guesses</strong>.
+        </p>
+    </div>
 }
 
 export default SuccessToasterComponent;
